d7760fe932244bc052954312fa4bd365d8a4de77
Bruna Sanches <bcpsanches@gmail.com>
2023-08-18 17:13:36 -0300

    correções quizz

--- Quiz/src/includes/script12.js
+++ Quiz/src/includes/script12.js
@@ -3,29 +3,42 @@
     var checkboxes = document.getElementsByName("opcao");
     for (var i = 0; i < checkboxes.length; i++) {
       checkboxes[i].checked = false;
+      document.getElementById("correta1").style.color="#404040";
+      document.getElementById("correta2").style.color="#404040";
+      document.getElementById("incorreta1").style.color="#404040";
+      document.getElementById("incorreta2").style.color="#404040";
     }
   });
   
   const checkboxes = document.getElementsByName("opcao");
-  
+ 
   
   
   
   for (let i = 0; i < checkboxes.length; i++) {
     checkboxes[i].addEventListener("change", function() {
-      if (checkboxes[0].checked) {
+      if (checkboxes[0].checked && checkboxes[1].checked) {
         alert("Parabéns! Você Acertou! Esta é uma reposição possível!\n\n Feedback: Tanto a escolha de repor 01 ampola quanto 02 ampolas são adequadas. A escolha vai depender da logística mais adequada de reposição. ");
-      }
-      else if ( checkboxes[1].checked) {
-        alert("Parabéns! Você Acertou! Esta é uma reposição possível!\n\n Feedback: Tanto a escolha de repor 01 ampola quanto 02 ampolas são adequadas. A escolha vai depender da logística mais adequada de reposição. ");
-      }
-     
-      else if(checkboxes[2].checked){
+        document.getElementById("correta1").style.color="green";
+        document.getElementById("correta2").style.color="green";
+      
+      }else if(checkboxes[2].checked && checkboxes[3].checked){
         alert("Resposta Incorreta! A dose sugerida é maior que a máxima permitida por dose. \n\n Clique no botão REINICIAR e tente novamente!");
+        document.getElementById("incorreta1").style.color="red";
+        document.getElementById("incorreta2").style.color="red";
+      }
+      else if(checkboxes[0].checked && checkboxes[2].checked){
+        alert("Apenas uma resposta está correta! \n\n Clique no botão REINICIAR e tente novamente!");
 
       }
-      else if(checkboxes[3].checked){
-        alert("Resposta Incorreta! A dose sugerida é maior que a máxima permitida por dose. \n\n Clique no botão REINICIAR e tente novamente!");
+      else if(checkboxes[0].checked && checkboxes[3].checked){
+        alert("Apenas uma resposta está correta! \n\n Clique no botão REINICIAR e tente novamente!");
+      }
+      else if(checkboxes[1].checked && checkboxes[2].checked){
+        alert("Apenas uma resposta está correta! \n\n Clique no botão REINICIAR e tente novamente!");
+      }
+      else if(checkboxes[1].checked && checkboxes[3].checked){
+        alert("Apenas uma resposta está correta! \n\n Clique no botão REINICIAR e tente novamente!");
       }
      
     });
